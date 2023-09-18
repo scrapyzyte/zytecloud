e88774522d92c17c2d46956194b26c69e7284dd4
scrapyzyte <scrapy.zyte@proton.me>
2023-09-18 13:16:12 +0200

    test dataclasses

--- brahmin_spider/items.py
+++ brahmin_spider/items.py
@@ -3,18 +3,36 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-from scrapy.item import Item, Field
+from dataclasses import dataclass, field
+from typing import Optional
 
 
-class BrahminSpiderItem(Item):
-    product_name = Field()
-    product_id = Field()
-    link = Field()
-    designer = Field()
-    color = Field()
-    price = Field()
-    sale_price = Field()
-    stock_status = Field()
-    image_urls = Field()
-    description = Field()
-    raw_description = Field()
+@dataclass
+class BrahminSpiderItem:
+    product_name: Optional[str] = field(default=None)
+    product_id: Optional[str] = field(default=None)
+    link: Optional[str] = field(default=None)
+    designer: Optional[str] = field(default=None)
+    color: Optional[str] = field(default=None)
+    price: Optional[float] = field(default=None)
+    sale_price: Optional[float] = field(default=None)
+    stock_status: Optional[str] = field(default=None)
+    image_urls: Optional[list] = field(default=None)
+    description: Optional[str] = field(default=None)
+    raw_description: Optional[str] = field(default=None)
+
+# from scrapy.item import Item, Field
+#
+#
+# class BrahminSpiderItem(Item):
+#     product_name = Field()
+#     product_id = Field()
+#     link = Field()
+#     designer = Field()
+#     color = Field()
+#     price = Field()
+#     sale_price = Field()
+#     stock_status = Field()
+#     image_urls = Field()
+#     description = Field()
+#     raw_description = Field()
